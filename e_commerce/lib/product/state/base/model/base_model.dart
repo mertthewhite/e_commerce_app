0295824c71ff7f7376fb560b3972f5abd1c10ae9
mertcan bora <merbora037@gmail.com>
2024-07-30 18:20:55 +0300

    .

--- e_commerce/lib/product/state/base/model/base_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class BaseModel<T> extends Equatable {
-  Map<String, Object?> toJson();
-  T fromJson(Map<String, Object?> json);
-
-  @override
-  List<Object?> get props => [];
-}
